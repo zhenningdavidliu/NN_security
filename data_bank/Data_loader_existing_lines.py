05e29cc125424a80b5c10a5bda470d17edd58fb6
Zhen Ning David Liu <znliu@nam-shub-01.uio.no>
2020-04-24 15:26:59 +0200

    updated existing lines

--- data_bank/Data_loader_existing_lines.py
+++ data_bank/Data_loader_existing_lines.py
@@ -15,25 +15,56 @@ grid_size (int): Size of images (grid_size x grid_size)
 Methods
 -------
 load_data(): Loads the training data.
+    
+Example arguments
+-----------------
+name: load_lines
+arguments:
+    number_of_samples: 10000
+    grid_size: 128
+    side_length: 30
+    width: 4
+    shift: True
+    color_shift: 0.01
+    save: True
+    images: data_train_images
+    labels: data_train_labels
+    difference: data_train_difference
+-----------------
+
 """
+ 
     def __init__(self, arguments):
         super(Data_loader_existing_lines, self).__init__(arguments)
-        required_keys = ['images','labels']
+        required_keys = ['images','labels','difference']
 
         self._check_for_valid_arguments(required_keys, arguments)
         self.images = arguments['images']
         self.labels = arguments['labels']
+        self.difference = arguments['difference']
 
     def load_data(self):
 
         images = self.images + ".npy"
         labels = self.labels + ".npy"
+        difference = self.difference + ".npy"
         images = join("data",images)
         labels = join("data",labels)
+        difference = join("data",difference)
         data = np.load(images)
         label = np.load(labels)
+        difference = np.load(difference)
        
-        data = np.expand_dims(data, axis =3)
+        #data = np.expand_dims(data, axis =3)
         
-        return data, label 
+        return data, label, difference
+
+def __str__(self):
+        class_str = """stripe data testing
+Images: %s 
+Labels: %s 
+Difference: %s 
+""" % ( self.images, self.labels, self.difference)
+        return class_str
+
 
